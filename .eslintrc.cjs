914dcbe8be60aae2c43565a34a9ef348a9cfb59c
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-07-12 09:58:26 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: ['react-app'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
